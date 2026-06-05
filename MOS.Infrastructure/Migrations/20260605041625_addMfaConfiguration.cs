0d549e19a6a62fa78cff50bfce2e95fd41478f79
ThanhLee <lequanthanhbp2019@gmail.com>
2026-06-05 11:17:46 +0700

    add migration mfa code

---
+++ MOS.Infrastructure/Migrations/20260605041625_addMfaConfiguration.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MOS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMfaConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MfaCodes_Users_UserId",
+                table: "MfaCodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MfaCodes_UserId",
+                table: "MfaCodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsUsed",
+                table: "MfaCodes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "MfaCodes",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "MfaCodes",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "EmailWhitelists",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MfaCodes_UserId_IsUsed_ExpiresAt",
+                table: "MfaCodes",
+                columns: new[] { "UserId", "IsUsed", "ExpiresAt" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MfaCodes_Users",
+                table: "MfaCodes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MfaCodes_Users",
+                table: "MfaCodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MfaCodes_UserId_IsUsed_ExpiresAt",
+                table: "MfaCodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsUsed",
+                table: "MfaCodes",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "MfaCodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "MfaCodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "EmailWhitelists",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MfaCodes_UserId",
+                table: "MfaCodes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MfaCodes_Users_UserId",
+                table: "MfaCodes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
